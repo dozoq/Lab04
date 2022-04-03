dcfff5320ac63a4fb54e6d7ed7016e0be2303d9a
dozoq <Sebastian.malek@fajne.to>
2022-04-03 13:05:23 +0200

    Repo Repair

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ver1;
 
 namespace Zadanie1
 {
@@ -6,7 +7,18 @@ namespace Zadanie1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(in doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+            
+            xerox.ScanAndPrint();
+            Console.WriteLine(xerox.Counter);
+            Console.WriteLine(xerox.PrintCounter);
+            Console.WriteLine(xerox.ScanCounter);
         }
     }
 }
