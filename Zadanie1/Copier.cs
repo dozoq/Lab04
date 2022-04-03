af82178418e456c23f7ef6edd901280453f02ce2
dozoq <Sebastian.malek@fajne.to>
2022-04-03 12:46:02 +0200

    First Task Completed

--- Zadanie1/Copier.cs
+++ Zadanie1/Copier.cs
@@ -5,39 +5,72 @@ namespace Zadanie1
 {
     public class Copier : BaseDevice, IPrinter, IScanner
     {
-        public int  PrintCounter { get; private set; }
-        public int  ScanCounter  { get; private set; }
-        public int  Counter      { get; private set; }
-        
-        public void Scan(out IDocument document, IDocument.FormatType formatType)
+        public int PrintCounter { get; private set; }
+        public int ScanCounter  { get; private set; }
+        public new int Counter      { get; private set; }
+
+        public new void PowerOn()
+        {
+            if (this.state == IDevice.State.off)
+            {
+                base.PowerOn();
+                Counter++;   
+            }
+        }
+
+        public new void PowerOff()
+        {
+            if (this.state == IDevice.State.@on)
+            {
+                base.PowerOff();
+            }
+        }
+
+
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.JPG)
         {
             string concat = "";
-            switch (formatType)
+            document = null;
+            if (this.state == IDevice.State.@on)
             {
-                case IDocument.FormatType.PDF:
-                    concat = $"PDFScan{ScanCounter}.pdf";
-                    break;
-                case IDocument.FormatType.JPG:
-                    concat = $"ImageScan{ScanCounter}.jpg";
-                    break;
-                case IDocument.FormatType.TXT:
-                    concat = $"TextScan{ScanCounter}.txt";
-                    break;
+                ScanCounter++;
+                switch (formatType)
+                {
+                    case IDocument.FormatType.PDF:
+                        concat   = $"PDFScan{ScanCounter}.pdf";
+                        document = new PDFDocument("");
+                        break;
+                    case IDocument.FormatType.JPG:
+                        concat   = $"ImageScan{ScanCounter}.jpg";
+                        document = new ImageDocument("");
+                        break;
+                    case IDocument.FormatType.TXT:
+                        concat   = $"TextScan{ScanCounter}.txt";
+                        document = new TextDocument("");
+                        break;
+                }
+
+                Console.WriteLine(concat);
             }
-            Console.WriteLine(concat);
         }
 
         public void Print(in IDocument document)
         {
-            Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
-            
+            if (this.state == IDevice.State.@on)
+            {
+                PrintCounter++;
+                Console.WriteLine($"{DateTime.Now} Print: {document.GetFileName()}");
+            }
         }
 
         public void ScanAndPrint()
         {
-            IDocument document;
-            Scan(out document, IDocument.FormatType.JPG);
-            Print(document);
+            if (this.state == IDevice.State.@on)
+            {
+                IDocument document;
+                Scan(out document, IDocument.FormatType.JPG);
+                Print(document);
+            }
         }
     }
 }
--- Zadanie1/Documents.cs
+++ Zadanie1/Documents.cs
@@ -1,24 +1,49 @@
-﻿using System.IO.Enumeration;
+using System;
 
-namespace Zadanie1
+namespace ver1
 {
-    public abstract class AbstractDocument: IDocument
+    public interface IDocument
     {
-        private string               fileName;
-        public  abstract IDocument.FormatType GetFormatType();
-        public string               GetFileName()
-        {
-            return fileName;
-        }
+        enum FormatType {TXT, PDF, JPG}
 
-        public AbstractDocument(string fileName)
-        {
-            this.fileName = fileName;
-        }
+        /// <summary>
+        /// Zwraca typ formatu dokumentu
+        /// </summary>
+        FormatType GetFormatType();
 
-        public void ChangeFileName(string fileName)
-        {
-            this.fileName = fileName;
-        }
+        /// <summary>
+        /// Zwraca nazwę pliku dokumentu - nie może być `null` ani pusty `string`
+        /// </summary>
+        string GetFileName();        
     }
+    public abstract class AbstractDocument : IDocument
+    {
+        private string fileName;
+        public AbstractDocument(string fileName) =>  this.fileName = fileName;
+
+        public string GetFileName() => fileName;
+
+        public void ChangeFileName(string newFileName) => fileName = newFileName;
+
+        public abstract IDocument.FormatType GetFormatType();
+    }
+
+    public class PDFDocument : AbstractDocument
+    {
+        public PDFDocument(string filename) : base(filename) { }
+        public override IDocument.FormatType GetFormatType() => IDocument.FormatType.PDF;        
+    }
+
+    public class ImageDocument : AbstractDocument
+    {
+        public ImageDocument(string filename) : base(filename) { }
+        public override IDocument.FormatType GetFormatType() => IDocument.FormatType.JPG;  
+    }
+
+    public class TextDocument : AbstractDocument
+    {
+        public TextDocument(string filename) : base(filename) { }
+        public override IDocument.FormatType GetFormatType() => IDocument.FormatType.TXT;  
+    }
+
 }
